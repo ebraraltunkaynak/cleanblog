2e1c8566802a2b29fd73a90447a5657776144a0f
Ebrar <16155052@mersin.edu.tr>
2022-08-12 16:32:38 +0300

    v2. ejs updated

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 
-// const express = require("express");
-// const app=express;
-
-
-// app.get('/',(req,res)=>{
-//     const blog = {
-//          id: 1,
-//           title: "Blog title",
-//            description: "Blog description" 
-//         };
-//         res.send(blog);
-           
-// });
-// const port =3000;
-
-// app.listen(port,()=>{
-//     console.log(`server $(port) portta başlatıldı..`);
-// });
-
 const express = require("express");
+const ejs = require("ejs")
+const path = require("ejs")
 
 const app = express();
 
+//template engine
+app.set("view engine","ejs");
+
+// MIDDLEWAres
+app.use(express.static('public'));
+
+//ROUTES
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render("index")
 });
+
+app.get("/about", (req, res) => {
+  res.render("about")
+});
+
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post")
+});
+
 
 
 const port = 3000;
