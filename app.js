0af2cbdd31941d8a2690f4e3b554951b07fd0f05
Ebrar <16155052@mersin.edu.tr>
2022-08-14 13:29:17 +0300

    database baglantısı yapıldı- css js de sıkıntı var

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 
 const express = require("express");
+const mongoose= require('mongoose');
 const ejs = require("ejs")
-const path = require("ejs")
+const path = require("path")
+const Post = require('./models/Post')
+
 
 const app = express();
+
+// connection database
+mongoose
+.connect('mongodb://localhost/cleanblog-test-db')
+.then(() => console.log('database bağlantısı kuruldu'));
 
 //template engine
 app.set("view engine","ejs");
 
 // MIDDLEWAres
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.render("index")
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {posts});
 });
-
 app.get("/about", (req, res) => {
   res.render("about")
 });
 
 
-app.get("/add_post", (req, res) => {
-  res.render("add_post")
+app.get("/post", (req, res) => {
+  res.render("post")
+});
+
+app.post("/add_post", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 
