91aa567519d773bb422e9112e6047651a4845c0a
Ebrar <16155052@mersin.edu.tr>
2022-08-15 14:02:01 +0300

    update delete mvc

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const mongoose=require('mongoose');
 const path=require('path');
 const ejs=require('ejs');
 const Blog=require('./models/Blog');
+const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 
+
+const BlogController=require('./controllers/BlogController')
+const PageController=require('./controllers/PageController')
 
 const app=express();
 
@@ -11,51 +16,34 @@ app.set('view engine', 'ejs');
 
 
 //connect mongodb
-mongoose.connect('mongodb://0.0.0.0:27017/cleanblog-test-db ')
+mongoose.connect('mongodb://localhost:27017/cleanblog-test-db');
+
 
 //MİDDLEWARES
 app.use(express.static(path.resolve(__dirname + '/public')));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-
+app.use(fileUpload())
+app.use(methodOverride('_method',{
+  methods:['POST','GET']
+}))
 
 
 //Routes
-app.get('/', async(req, res) => {
-  const posts= await Blog.find({})
-    res.render('index', {
-      posts
-    })
-  });
-
-  app.get('/posts/:id', async(req, res) => {
-    const post=await Blog.findById(req.params.id)
-    res.render('post',{
-      post
-    })
-  });
-  
-  
-  app.get('/about', (req, res) => {
-    res.render('about');
-  });
-  
-  app.get('/add_post', async (req, res) => {
-    res.render('add_post')
-  });
-  
-  app.get('/post', (req, res) => {
-    res.render('post');
-  });
-
-app.post('/posts',async(req,res)=>{
-  Blog.create(req.body)
-  res.redirect('/')
-})
+app.get('/',BlogController.getAllBlog);
+app.get('/posts/:id',BlogController.getBlog);
+app.get('/about',PageController.getABout);
+app.get('/add_post',PageController.getAddNewpost );
+app.get('/posts/edit/:id',PageController.getEditPost )
+app.post('/posts',BlogController.getCreatepost)
+app.delete('/post/:id',BlogController.deletePost)
+app.put('/posts/:id',BlogController.updatePost)
 
 
 
-const port = 3000;
+
+const port = 3000
+
 app.listen(port, () => {
-  console.log(`server ${port} portunda çalışmaya başladı`);
-});
+    console.log(`Sunucu ${port} portunda başlatıldı`);
+})
